efa43e8860e80dd338fc45ed083cbd1af1f36cdf
Ashutosh_Rai_78 <ashutosh.devel0oper78@gmail.com>
2022-05-24 10:19:50 -0400

    Final Commit

---
+++ lib/SecondPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(SecondPage());
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
